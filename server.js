89ddae4771aafb9a6c6e3185aa8fca8305fec794
Minh Duy <126150563+minhduy0909@users.noreply.github.com>
2023-09-11 12:55:27 +0700

    hello world with express

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const hostname='127.0.0.1'
-const port =3000
-const server = http.createServer((req,res)=>{
-    res.statusCode=200
-    res.setHeader('Context-Type','text/plain')
-    res.end('Chào mừng!!')
-})
+const express = require('express');
+const app = express();
+const port = 3000
 
-server.listen(port,hostname,()=>{
-    console.log(`Server is running on http://${hostname}:${port}`);
+app.get('/',(req,res)=>{
+    res.send("Hello ");
+
+})
+app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`);
 })
