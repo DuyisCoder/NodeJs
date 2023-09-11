3f2bb81d775f4874df0946eebddb5a7e6fb2a518
Minh Duy <126150563+minhduy0909@users.noreply.github.com>
2023-09-11 14:07:12 +0700

    We config static files and install nodemon

--- src/server.js
+++ src/server.js
@@ -1,24 +1,29 @@
-const express = require('express');
-const path=require('path');
-const app = express();
+const express = require('express')
+const app = express()
+const path=require('path')
 
-// if want use file env we can install dotenv and init it
-require('dotenv').config();
-const port = process.env.PORT || 9090 // guard port is underfined
+// if you want use file .env we can install library dotenv and init it
+require('dotenv').config()
+const port = process.env.PORT || 8000
 const hostname=process.env.HOST_NAME
 
-app.get('/',(req,res)=>{
-    res.send("Hello ");
-})
-
-// Config template engine
-app.set('views',path.join(__dirname,'views')) // tìm đường dẫn tới route views
+// config template engine
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+// config static files: so 'public' -> it will search from path public onwards
+app.use(express.static(path.join(__dirname,'public')));
 
+// Init routes
+app.get('/',(req,res)=>{
+    res.render('sample.ejs');
+})
 app.get('/admin',(req,res)=>{
-    res.render('sample.ejs')
+    res.send('Is admin!!');
 })
 app.listen(port,()=>{
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port ${port}`);
 })
+
+
+
